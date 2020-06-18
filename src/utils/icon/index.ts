8bb7f391d4e42078ab3bf091fc2015cf1df54558
DevilTea <ch870814@gmail.com>
2020-06-18 22:18:07 +0800

    Remove auto-generated files

--- src/utils/icon/index.ts
+++
@@ -1,6 +0,0 @@
-import { fontAwesomeIcons, FontAwesomeSource, FontAwesomeIconData } from './icons/fontAwesome'
-export * from './icons/fontAwesome'
-export type IconSource = FontAwesomeSource
-export const icons: (FontAwesomeIconData)[] = [
-  ...fontAwesomeIcons
-]
