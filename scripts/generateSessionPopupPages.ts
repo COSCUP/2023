f72de059510d9764d4c2e1e20e2624dbef25d2a6
DevilTea <ch870814@gmail.com>
2020-06-23 06:05:43 +0800

    Refactor generateSessionPopupPages.ts

--- scripts/generateSessionPopupPages.ts
+++ scripts/generateSessionPopupPages.ts
@@ -7,13 +7,9 @@ import cheerio from 'cheerio'
 import fs from 'fs'
 import path from 'path'
 import { createAgendaService } from '@/services/agenda'
+import { availableLanguageTypes } from '@/services/language'
 import { MetaDomSetterSet, MetaType, defaultMetaValues, createMetaService } from '@/services/meta'
 import { GeneralPopupContentData } from '@/services/popup'
-
-const zhOriginalHtmlDir = path.join(__dirname, '../dist/2020/zh-TW/agenda/')
-const zhOriginalHtml = fs.readFileSync(path.join(zhOriginalHtmlDir, 'template.html')).toString()
-const enOriginalHtmlDir = path.join(__dirname, '../dist/2020/en/agenda/')
-const enOriginalHtml = fs.readFileSync(path.join(enOriginalHtmlDir, 'template.html')).toString()
 
 function createCheerioMetaDomSetterSet ($: CheerioStatic) {
   const cheerioMetaDomSetterSet: MetaDomSetterSet = {
@@ -53,47 +49,35 @@ async function run () {
     agendaService.dayIndex = index
   })
 
-  // load all sessions' popup data
-  let datas = await Promise.all(
-    Object.keys(agendaService.sessionSet)
-      .map(async (sessionId) => {
-        return {
-          sessionId,
-          popupData: await agendaService.getSessionPopupData(sessionId, 'zh')
-        }
-      })
-  )
-
-  datas.forEach((data) => {
-    const $ = cheerio.load(zhOriginalHtml, { _useHtmlParser2: true })
-    const metaService = createMetaService(createCheerioMetaDomSetterSet($))
-    metaService.setMeta(data.popupData.metaOptions)
-    $('#session-detail').replaceWith((data.popupData.contentData as GeneralPopupContentData).html)
-    fs.writeFileSync(path.join(zhOriginalHtmlDir, `${data.sessionId}.html`), $.html())
-    fs.mkdirSync(path.join(zhOriginalHtmlDir, data.sessionId))
-    fs.writeFileSync(path.join(zhOriginalHtmlDir, data.sessionId, 'index.html'), $.html())
+  await Promise.all(availableLanguageTypes.map((languageType) => {
+    return {
+      languageType: languageType,
+      languageTypeAlias: (languageType === 'zh-TW' ? 'zh' : 'en') as 'zh' | 'en',
+      outputHtmlDir: path.join(__dirname, `../dist/2020/${languageType}/agenda/`),
+      templateHtml: fs.readFileSync(path.join(__dirname, `../dist/2020/${languageType}/agenda/template.html`)).toString()
+    }
   })
+    .map(async (languageData) => {
+      const datas = await Promise.all(
+        Object.keys(agendaService.sessionSet)
+          .map(async (sessionId) => {
+            return {
+              sessionId,
+              popupData: await agendaService.getSessionPopupData(sessionId, languageData.languageTypeAlias)
+            }
+          })
+      )
 
-  // load all sessions' popup data
-  datas = await Promise.all(
-    Object.keys(agendaService.sessionSet)
-      .map(async (sessionId) => {
-        return {
-          sessionId,
-          popupData: await agendaService.getSessionPopupData(sessionId, 'en')
-        }
+      datas.forEach((data) => {
+        const $ = cheerio.load(languageData.templateHtml, { _useHtmlParser2: true })
+        const metaService = createMetaService(createCheerioMetaDomSetterSet($))
+        metaService.setMeta(data.popupData.metaOptions)
+        $('#session-detail').replaceWith((data.popupData.contentData as GeneralPopupContentData).html)
+        fs.writeFileSync(path.join(languageData.outputHtmlDir, `${data.sessionId}.html`), $.html())
+        fs.mkdirSync(path.join(languageData.outputHtmlDir, data.sessionId))
+        fs.writeFileSync(path.join(languageData.outputHtmlDir, data.sessionId, 'index.html'), $.html())
       })
-  )
-
-  datas.forEach((data) => {
-    const $ = cheerio.load(enOriginalHtml, { _useHtmlParser2: true })
-    const metaService = createMetaService(createCheerioMetaDomSetterSet($))
-    metaService.setMeta(data.popupData.metaOptions)
-    $('#session-detail').replaceWith((data.popupData.contentData as GeneralPopupContentData).html)
-    fs.writeFileSync(path.join(enOriginalHtmlDir, `${data.sessionId}.html`), $.html())
-    fs.mkdirSync(path.join(enOriginalHtmlDir, data.sessionId))
-    fs.writeFileSync(path.join(enOriginalHtmlDir, data.sessionId, 'index.html'), $.html())
-  })
+    }))
 }
 
 run()
