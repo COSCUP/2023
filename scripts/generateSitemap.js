a2a50f208bbe057c8e7d685e42e46fd59cd729c1
DevilTea <ch870814@gmail.com>
2020-06-23 07:57:03 +0800

    Add cli spinner for sitemap generating process

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -7,13 +7,14 @@ const app = new (require('koa'))()
 const port = 3000
 const origin = require('../package.json').origin
 const sitemapPath = path.join(__dirname, '../dist/sitemap.xml')
+const spinner = require('ora')('Generating sitemap.xml...').start()
 
 app.use(serve({ rootDir: path.join(__dirname, '../dist'), rootPath: '/2020' }))
 const generator = SitemapGenerator(`http://localhost:${port}/2020/`, {
   filepath: sitemapPath,
   maxEntriesPerFile: 50000,
   stripQuerystring: false,
-  piorityMap: [1.0, 0.8, 0.64, 0.4, 0.2, 0],
+  priorityMap: [1.0, 0.8, 0.64, 0.4, 0.2, 0],
   lastMod: true
 })
 
@@ -24,8 +25,9 @@ const server = app.listen(port, () => {
 generator.on('done', () => {
   server.close()
   fs.writeFileSync(sitemapPath, fs.readFileSync(sitemapPath).toString().replace(new RegExp(`http://localhost:${port}`, 'g'), origin))
+  spinner.succeed('sitemap.xml generated.')
 })
 
 generator.on('add', (url) => {
-  console.log(`Add ${origin}${url.split(`http://localhost:${port}`)[1]}`)
+  spinner.text = `Add ${origin}${url.split(`http://localhost:${port}`)[1]}`
 })
