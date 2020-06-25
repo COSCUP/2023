a6e08aaff27dcb3ed6fa8d8bc4ac322bd08624ea
DevilTea <ch870814@gmail.com>
2020-06-25 15:21:57 +0800

    Add Sponsor page

--- src/pages/Sponsor.vue
+++ src/pages/Sponsor.vue
@@ -7,20 +7,88 @@
 
 <template>
   <main id="sponsor" class="page-container">
-    Sponsor
+    <div class="card outer-container">
+      <h2 class="title">
+        {{ languageService.languagePack.sponsor.callForSponsorship.title }}
+      </h2>
+      <p class="call-for-sponsorship">
+        {{ languageService.languagePack.sponsor.callForSponsorship.content }}
+        <a href="mailto:sponsorship@coscup.org">
+          sponsorship@coscup.org
+        </a>
+      </p>
+    </div>
+    <div
+      v-for="level in Object.keys(sponsorGroups)"
+      :key="`sponsor-level-${level}`"
+      class="outer-container"
+    >
+      <h2 class="title level">
+        {{ languageService.languagePack.sponsor.level[level] }}
+      </h2>
+      <div
+        v-for="sponsor in sponsorGroups[level]"
+        :key="sponsor.id"
+        class="card sponsor-container"
+      >
+        <div class="img-container">
+          <a :href="`${sponsor.link}`" target="_blank" rel="noopener">
+            <img
+              :src="`/2020/images/sponsors/${sponsor.image}`"
+              :alt="
+                `Sponsor ${sponsor.name[languageService.languageType]}'s logo`
+              "
+            />
+          </a>
+        </div>
+        <div class="content-container">
+          <a :href="`${sponsor.link}`" target="_blank" rel="noopener">
+            <h2>
+              {{ sponsor.name[languageService.languageType] }}
+            </h2>
+          </a>
+          <article
+            v-html="sponsor.intro[languageService.languageType]"
+            class="markdown"
+          ></article>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { groupBy } from 'lodash-es'
+import { availableLanguageTypes } from '@/services/language'
+import markdown from '@/utils/markdown'
+import sponsorDatas from '@/../public/json/sponsor.json'
 
 import '@/assets/scss/pages/sponsor.scss'
 
+export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType[number];
+type SopnsorData = ArrayElement<typeof sponsorDatas>
+
 export default Vue.extend({
   name: 'Sponsor',
-  components: {
+  inject: ['languageService'],
+  data () {
+    return {
+      sponsorGroups: {} as object
+    }
   },
-  mounted () {
+  methods: {
+    async initSponsors () {
+      for (const data of sponsorDatas) {
+        for (const languageType of availableLanguageTypes) {
+          data.intro[languageType] = await markdown(data.intro[languageType])
+        }
+      }
+      this.sponsorGroups = groupBy<SopnsorData>(sponsorDatas, 'level')
+    }
+  },
+  async mounted () {
+    await this.initSponsors()
     this.$dispatchRenderedEvent()
   }
 })
