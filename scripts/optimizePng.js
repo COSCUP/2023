4bed58cfabaafda44b456835b9e226f108788e6d
DevilTea <ch870814@gmail.com>
2020-08-01 17:32:57 +0800

    Update optimize image script

--- scripts/optimizePng.js
+++ scripts/optimizePng.js
@@ -4,21 +4,31 @@ const { execFile } = require('child_process')
 const optipng = require('optipng-bin')
 const find = require('find')
 const path = require('path')
+const ora = require('ora')
 
 const execFileP = promisify(execFile)
 
+const optimizeOne = async (file) => {
+  const spinner = ora(`Optimizing PNG: ${file}...`).start()
+  const { stderr } = await execFileP(optipng, ['-o9', file])
+
+  if (stderr && !['already optimized'].some((reason) => stderr.includes(reason))) {
+    spinner.fail(`Error when optimizing PNG: ${file}`)
+    console.log(stderr)
+  } else {
+    spinner.succeed(`Optimized PNG: ${file}!`)
+  }
+}
+
+// const delay = () => new Promise(resolve => setTimeout(resolve, 300))
+
 async function run () {
   const files = find.fileSync(/\.png$/, path.join(__dirname, '..'))
-    .filter((file) => !file.includes('node_modules'))
-  await Promise.all(files.map(async (file) => {
-    const spinner = require('ora')(`Optimizing PNG: ${file}...`)
-    try {
-      await execFileP(optipng)
-      spinner.succeed(`Optimized PNG: ${file}!`)
-    } catch (_) {
-      spinner.fail(`Error when optimizing PNG: ${file}`)
-    }
-  }))
+    .filter((file) => !['/node_modules/', '/dist/'].some((ignore) => file.includes(ignore)))
+
+  for (const file of files) {
+    await optimizeOne(file)
+  }
   console.log('Done!')
 }
 
