fc2cdffaa8ed8c1b64878e6159b55aaff816e27f
DevilTea <ch870814@gmail.com>
2020-10-12 00:31:13 +0800

    Update generate-sitemap script

--- scripts/generateSitemap.js
+++ scripts/generateSitemap.js
@@ -2,21 +2,13 @@
 const fs = require('fs')
 const path = require('path')
 const SitemapGenerator = require('sitemap-generator')
-const serve = require('koa-static-server')
-const app = new (require('koa'))()
-const port = 3000
-const origin = require('../package.json').origin
-const sitemapPath = path.join(__dirname, '../dist/sitemap.xml')
+const sitemapPath = path.join(__dirname, '../public/sitemap.xml')
 const spinner = require('ora')('Generating sitemap.xml...')
-let serverOnServe = () => { throw new Error('Promise is not init') }
-const waitForServer = new Promise((resolve) => {
-  serverOnServe = resolve
-})
-
-app.use(serve({ rootDir: path.join(__dirname, '../dist'), rootPath: '/2020' }))
-
-const server = app.listen(port, serverOnServe)
-const generator = SitemapGenerator(`http://localhost:${port}/2020/`, {
+const port = 3000
+const isLocal = process.argv[2] === '--local'
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+const siteUrl = isLocal ? `http://localhost:${port}/2020/` : 'https://coscup.org/2020/'
+const generator = SitemapGenerator(siteUrl, {
   filepath: sitemapPath,
   maxEntriesPerFile: 50000,
   stripQuerystring: false,
@@ -24,24 +16,59 @@ const generator = SitemapGenerator(`http://localhost:${port}/2020/`, {
   lastMod: true
 })
 
-generator.on('error', (error) => {
-  console.log(error)
-  // => { code: 404, message: 'Not found.', url: 'http://example.com/foo' }
-})
+async function setupGeneratorForLocal (generator) {
+  const serve = require('koa-static-server')
+  const app = new (require('koa'))()
+  const origin = require('../package.json').origin
 
-generator.on('done', () => {
-  server.close()
-  const content = fs.readFileSync(sitemapPath).toString()
-  fs.writeFileSync(sitemapPath, content.replace(new RegExp(`http://localhost:${port}`, 'g'), origin))
-  spinner.succeed('sitemap.xml generated.')
-})
-
-generator.on('add', (url) => {
-  spinner.text = `Add ${origin}${url.split(`http://localhost:${port}`)[1]}`
-})
-
-waitForServer
-  .then(() => {
-    spinner.start()
-    generator.start()
+  let serverOnServe = () => { throw new Error('Promise is not init') }
+  const waitForServer = new Promise((resolve) => {
+    serverOnServe = resolve
   })
+
+  app.use(serve({ rootDir: path.join(__dirname, '../dist'), rootPath: '/2020' }))
+
+  const server = app.listen(port, serverOnServe)
+
+  generator.on('error', (error) => {
+    console.log(error)
+    // => { code: 404, message: 'Not found.', url: 'http://example.com/foo' }
+  })
+
+  generator.on('done', () => {
+    server.close()
+    const content = fs.readFileSync(sitemapPath).toString()
+    fs.writeFileSync(sitemapPath, content.replace(new RegExp(`http://localhost:${port}`, 'g'), origin))
+    spinner.succeed('sitemap.xml generated.')
+  })
+
+  generator.on('add', (url) => {
+    spinner.text = `Add ${origin}${url.split(`http://localhost:${port}`)[1]}`
+  })
+
+  await waitForServer
+}
+
+async function setupGeneratorForOnline (generator) {
+  generator.on('error', (error) => {
+    console.log(error)
+    // => { code: 404, message: 'Not found.', url: 'http://example.com/foo' }
+  })
+
+  generator.on('done', () => {
+    spinner.succeed('sitemap.xml generated.')
+  })
+
+  generator.on('add', (url) => {
+    spinner.text = `Add ${url}`
+  })
+}
+
+async function generateSitemap () {
+  await (isLocal ? setupGeneratorForLocal : setupGeneratorForOnline)(generator)
+  console.log(`Generate from ${isLocal ? 'local' : 'online'}`)
+  spinner.start()
+  generator.start()
+}
+
+generateSitemap()
