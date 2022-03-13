94ebdb4e016d102801e1ce7e55e2df662c45bde6
yoyo930021 <yoyo930021@gmail.com>
2022-03-14 00:05:44 +0800

    Fix path_path format

--- src/modules/gtag/index.ts
+++ src/modules/gtag/index.ts
@@ -16,7 +16,7 @@ export const setup: UserModuleSetup = ({ router }) => {
   trackRouter(router, {
     template: (to) => ({
       page_title: to.name,
-      page_path: import.meta.env.BASE_URL + to.path,
+      page_path: import.meta.env.BASE_URL.replace(/\/$/, '') + to.path,
       page_location: window.location.href
     })
   })
