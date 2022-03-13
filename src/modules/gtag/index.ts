df50dcac01696e4432cbdad3120967e234ead01f
yoyo930021 <yoyo930021@gmail.com>
2022-03-13 23:52:18 +0800

    Track router change

--- src/modules/gtag/index.ts
+++ src/modules/gtag/index.ts
@@ -1,5 +1,5 @@
-import { UserModuleInstall } from '@/modules/types'
-import VueGtag from 'vue-gtag-next'
+import { UserModuleInstall, UserModuleSetup } from '@/modules/types'
+import VueGtag, { trackRouter } from 'vue-gtag-next'
 
 export const install: UserModuleInstall = ({ app }) => {
   if (!import.meta.env.VITE_GA_ID) return
@@ -10,4 +10,8 @@ export const install: UserModuleInstall = ({ app }) => {
       id: import.meta.env.VITE_GA_ID
     }
   })
+}
+
+export const setup: UserModuleSetup = ({ router }) => {
+  trackRouter(router)
 }
