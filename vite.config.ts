b95f0b6f8d086a405962b9ccc552ee312affaf36
yoyo930021 <yoyo930021@gmail.com>
2022-07-29 13:36:48 +0800

    Fix navigate fallback

--- vite.config.ts
+++ vite.config.ts
@@ -69,6 +69,7 @@ export default defineConfig(({ mode, command }) => {
         registerType: 'autoUpdate',
         workbox: {
           navigateFallback: 'index.html',
+          navigateFallbackDenylist: [/.*\.jpg$/, /.*\.png$/, /.*\.svg$/, /.*\.json$/, /.*\.js$/],
           offlineGoogleAnalytics: true,
           runtimeCaching: [
             {
