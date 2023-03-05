d7d6048b7c1adc1bb068e232cade7f23f418a4c0
Mirumo <mysteryperson520@gmail.com>
2023-03-05 08:36:43 +0800

    Feat: collect session and my collection page

---
+++ src/pages/Collection.vue
@@ -0,0 +1,31 @@
+<template>
+  <main id="collection" class="page-container">
+    <h1 class="caption">
+      <span>我的收藏</span>
+    </h1>
+    <article v-for="id in sessions" :sessionId="id" :key="id" class="session-list">
+      <ScheduleItem :sessionId="id"></ScheduleItem>
+    </article>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useRoute } from 'vue-router'
+import ScheduleItem from '@/components/Session/ScheduleItem.vue'
+import '@/assets/scss/pages/collection.scss'
+
+export default defineComponent({
+  name: 'SessionMark',
+  components: {
+    ScheduleItem
+  },
+  setup () {
+    const route = useRoute()
+    const sessions = route.query.sessions?.toString().split('-') ?? JSON.parse(window.localStorage.getItem('MARK_SESSIONS') ?? '[]')
+
+    return { sessions }
+  }
+})
+
+</script>
