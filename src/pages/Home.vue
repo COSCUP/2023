31408493866a6ed4a482c637a2173a686b27b090
Spyup <jason88tu@gmail.com>
2023-03-31 03:50:14 +0000

    Take out url of youtube stream

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -14,19 +14,19 @@
     <div class="info-container">
       <span class="date">{{startDate}} ~ {{endDate}}</span>
       <div class="info">
-        <a class="venue" href="https://youtu.be/MaQuXvkR7to" target="_blank">
+        <a class="venue" :href=day1RB target="_blank">
           {{ t('home.info.day_1_rb_online') }}
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1.0em" height="1.0em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512" class="suffix-icon"><path d="M432 320h-32a16 16 0 0 0-16 16v112H64V128h144a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16H48a48 48 0 0 0-48 48v352a48 48 0 0 0 48 48h352a48 48 0 0 0 48-48V336a16 16 0 0 0-16-16zM488 0H360c-21.37 0-32.05 25.91-17 41l35.73 35.73L135 320.37a24 24 0 0 0 0 34L157.67 377a24 24 0 0 0 34 0l243.61-243.68L471 169c15 15 41 4.5 41-17V24a24 24 0 0 0-24-24z" fill="currentColor"></path></svg>
         </a>
-        <a class="venue" href="https://youtu.be/YexUnVOZC8M" target="_blank">
+        <a class="venue" :href=day1AU target="_blank">
           {{ t('home.info.day_1_au_online') }}
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1.0em" height="1.0em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512" class="suffix-icon"><path d="M432 320h-32a16 16 0 0 0-16 16v112H64V128h144a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16H48a48 48 0 0 0-48 48v352a48 48 0 0 0 48 48h352a48 48 0 0 0 48-48V336a16 16 0 0 0-16-16zM488 0H360c-21.37 0-32.05 25.91-17 41l35.73 35.73L135 320.37a24 24 0 0 0 0 34L157.67 377a24 24 0 0 0 34 0l243.61-243.68L471 169c15 15 41 4.5 41-17V24a24 24 0 0 0-24-24z" fill="currentColor"></path></svg>
         </a>
-        <a class="venue" href="https://youtu.be/87qYQQ3pHFc" target="_blank">
+        <a class="venue" :href=day2RB target="_blank">
           {{ t('home.info.day_2_rb_online') }}
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1.0em" height="1.0em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512" class="suffix-icon"><path d="M432 320h-32a16 16 0 0 0-16 16v112H64V128h144a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16H48a48 48 0 0 0-48 48v352a48 48 0 0 0 48 48h352a48 48 0 0 0 48-48V336a16 16 0 0 0-16-16zM488 0H360c-21.37 0-32.05 25.91-17 41l35.73 35.73L135 320.37a24 24 0 0 0 0 34L157.67 377a24 24 0 0 0 34 0l243.61-243.68L471 169c15 15 41 4.5 41-17V24a24 24 0 0 0-24-24z" fill="currentColor"></path></svg>
         </a>
-        <a class="venue" href="https://youtu.be/3a-bbpf2CcI" target="_blank">
+        <a class="venue" :href=day2AU target="_blank">
           {{ t('home.info.day_2_au_online') }}
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1.0em" height="1.0em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 512 512" class="suffix-icon"><path d="M432 320h-32a16 16 0 0 0-16 16v112H64V128h144a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16H48a48 48 0 0 0-48 48v352a48 48 0 0 0 48 48h352a48 48 0 0 0 48-48V336a16 16 0 0 0-16-16zM488 0H360c-21.37 0-32.05 25.91-17 41l35.73 35.73L135 320.37a24 24 0 0 0 0 34L157.67 377a24 24 0 0 0 34 0l243.61-243.68L471 169c15 15 41 4.5 41-17V24a24 24 0 0 0-24-24z" fill="currentColor"></path></svg>
         </a>
@@ -81,6 +81,11 @@ export default defineComponent({
     const sections = ref<Section[]>([])
     const startDate = import.meta.env.VITE_START_DATE
     const endDate = import.meta.env.VITE_END_DATE
+    const day1RB = String(import.meta.env.VITE_DAY1_RB)
+    const day1AU = String(import.meta.env.VITE_DAY1_AU)
+    const day2RB = String(import.meta.env.VITE_DAY2_RB)
+    const day2AU = String(import.meta.env.VITE_DAY2_AU)
+
     watch(locale, async () => {
       sections.value = [
         {
@@ -107,7 +112,11 @@ export default defineComponent({
       t,
       sections,
       startDate,
-      endDate
+      endDate,
+      day1RB,
+      day1AU,
+      day2RB,
+      day2AU
     }
   }
 })
